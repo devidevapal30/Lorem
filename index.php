ef4f8f7707a3b0628044769f595eda64a7ab363c
devidevapal30 <devidevapal30@gmail.com>
2021-04-01 19:05:56 +0530

    Lorem

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Page</title>
+</head>
+<body>
+    <div class="container">
+    <div class="row">
+    <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6"> 
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium placeat alias sapiente dolorem dolores dolore delectus repellendus. Recusandae cupiditate similique dolor provident, expedita vel aliquid, voluptas sit doloribus, ullam temporibus.
+         </div>
+    <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6"> 
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae id obcaecati, est eos perspiciatis esse aliquid maxime dignissimos exercitationem hic quae. Reiciendis id expedita ut facere fugit incidunt non quis. 
+    </div> 
+    <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6"> 
+       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam ipsum aliquid reiciendis ab perspiciatis quis dolore eligendi odit temporibus modi natus dignissimos dicta, culpa autem at voluptate voluptatibus eaque! Quia.
+    </div>
+    </div>
+    </div>
+</body>
+</html>
